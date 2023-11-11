d93f85fbe2888bfc30f606f5da0729f94ed8b7a2
Javier Fernández <leqjl93@hotmail.com>
2023-11-10 23:44:09 -0600

    x

--- src/js/flyerViewer.vue.js
+++ src/js/flyerViewer.vue.js
@@ -23,7 +23,7 @@ const FlyerViewer = {
     },
     template : `
         <div class="flyer-container gx-0 row justify-content-center mb-5 flyer-container gx-0 row rounded overflow-hidden">
-           <div @click="goToEvent(banner)" v-for="banner in banners" class="col-12 col-xl-6 cursor-pointer">
+           <div @click="goToEvent(banner)" v-for="banner in banners" class="col cursor-pointer">
                 <img :src="banner.image" class="img-fluid">
             </div>
         </div>
