375da069ee457b40a2c765e3f7b20d91092a5cd7
Javier Fernández <leqjl93@hotmail.com>
2023-11-12 14:43:42 -0600

    cache

--- src/js/adminkyc.vue.js
+++ src/js/adminkyc.vue.js
@@ -1,4 +1,4 @@
-import { AdminkycViewer } from '../../src/js/adminkycViewer.vue.js?v=2.3.8'   
+import { AdminkycViewer } from '../../src/js/adminkycViewer.vue.js?v=2.3.9'   
 
 Vue.createApp({
     components : { 
