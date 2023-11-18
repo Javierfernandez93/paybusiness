ffe251555c925dc62ac4dd62ca7bb33651e0ed14
Javier Fernández <leqjl93@hotmail.com>
2023-11-17 19:19:20 -0600

    cache

--- src/js/rangewidgetViewer.vue.js
+++ src/js/rangewidgetViewer.vue.js
@@ -1,4 +1,4 @@
-import { User } from '../../src/js/user.module.js?v=2.5.1'   
+import { User } from '../../src/js/user.module.js?v=2.5.6'   
 
 const RangewidgetViewer = {
     name : 'rangewidget-viewer',
