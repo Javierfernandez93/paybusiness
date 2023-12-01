092728d006836d4c91f50ca3eebc33d43c3adb72
Javier Fernández <leqjl93@hotmail.com>
2023-12-01 15:32:50 -0600

    Create test.php

---
+++ apps/academy/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>x</title>
+
+</head>
+
+<body>
+<iframe title="vimeo-player" src="https://player.vimeo.com/video/890122421?h=ef3219a04e" width="640" height="360" frameborder="0"    allowfullscreen></iframe>
+</body>
+
+</html>
