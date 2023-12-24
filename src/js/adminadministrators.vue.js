788165536f3d69fc79bad0a8eb4ad717a09fcb3b
Javier Fernández <leqjl93@hotmail.com>
2023-12-23 22:47:33 -0600

    x

--- src/js/adminadministrators.vue.js
+++ src/js/adminadministrators.vue.js
@@ -1,4 +1,4 @@
-import { AdminadministratorsViewer } from '../../src/js/adminadministratorsViewer.vue.js?v=1.0.1'
+import { AdminadministratorsViewer } from '../../src/js/adminadministratorsViewer.vue.js?v=1.0.2'
 
 Vue.createApp({
     components : { 
