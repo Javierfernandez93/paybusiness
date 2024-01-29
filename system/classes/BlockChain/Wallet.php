d9e15217165d68568a2c99d70ff6d2082f280480
Javier Fernández <leqjl93@hotmail.com>
2024-01-29 16:24:55 -0600

    Update Wallet.php

--- system/classes/BlockChain/Wallet.php
+++ system/classes/BlockChain/Wallet.php
@@ -257,6 +257,12 @@ class Wallet extends Orm
 
 	public function createTransaction(string $recipient_adress = null,float $amount = null,string $data = '',bool $get_txid = false,float $fee = 0) 
 	{
+		d([
+			$this->getBalance(),
+			$amount,
+			$data
+		]);
+		
 		if($this->getBalance() >= $amount)
 		{
 			$feeAmout = 0;
