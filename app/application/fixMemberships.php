8eab84770478b21f520effe04a4d9733dcbdde78
Javier Fernández <leqjl93@hotmail.com>
2024-02-15 19:33:04 -0600

    Create fixMemberships.php

---
+++ app/application/fixMemberships.php
@@ -0,0 +1,29 @@
+<?php define("TO_ROOT", "../../");
+
+require_once TO_ROOT. "/system/core.php";
+
+$data = HCStudio\Util::getHeadersForWebService();
+
+$BuyPerUser = new Unlimited\BuyPerUser;
+$buys = $BuyPerUser->findAll("amount = ? AND catalog_payment_method_id = ? AND status = ?",[20,7,2],["user_login_id","amount"]);
+
+if($buys)
+{
+    foreach($buys as $buy)
+    {
+        $MembershipPerUser = new Unlimited\MembershipPerUser;
+        
+        if(!$MembershipPerUser->findField("user_login_id = ? AND catalog_membership_id = ?",[$buy['user_login_id'],1],"membership_per_user_id"))
+        {
+            echo "Creandolo para {$buy['user_login_id']}\n";
+
+            Unlimited\BuyPerUser::addMembership([
+                'point' => $buy['amount'],
+                'catalog_membership_id' => 1,
+                'user_login_id' => $buy['user_login_id'],
+            ]);
+        }
+    }
+}   
+
+echo json_encode(HCStudio\Util::compressDataForPhone($data)); 
