eef66bd3f9aa102244e590bedbc5fc3d1af97bd5
Javier Fernández <leqjl93@hotmail.com>
2024-02-15 18:29:53 -0600

    Update paybusinesswidgetViewer.vue.js

--- src/js/paybusinesswidgetViewer.vue.js
+++ src/js/paybusinesswidgetViewer.vue.js
@@ -56,23 +56,15 @@ const PaybusinesswidgetViewer = {
             </div>
         </div>
         <div v-else-if="membership == false" class="card shadow-none card-height-special overflow-hidden border">
-            <div class="mask bg-dark d-flex d-none justify-content-center align-items-center text-center text-white z-index-1">
-                <div class="row">
-                    <div class="col-12">
-                        <div><i class="bi h1 text-white bi-lock-fill"></i></div>
-                        Activate para comenzar
-                    </div>
-                </div>
-            </div>
             <div class="card-body py-5 text-center position-relative z-index-0">
                 <div class="row mb-3">
                     <div class="col-12 col-xl">
                         <h1 class="text-dark sans">Pay Business</h1>
-                        <h3 class="text-secondary fw-semilight">Membresía 20 USDT</h3>
+                        <h3 class="text-secondary fw-semilight">Activa tu licencia</h3>
                     </div>
                 </div>
                 <div class="progress" style="height:1rem" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                    <div style="height:1rem" class="progress-bar bg-success" style="width: 1%">1%</div>
+                    <div style="height:1rem" class="progress-bar bg-success" style="width: 0%">0%</div>
                 </div>
             </div>
         </div>
