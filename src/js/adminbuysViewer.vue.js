85070c78113fa37bb51453169950c1ba54c2613c
Javier Fernández <leqjl93@hotmail.com>
2024-03-08 10:29:05 -0600

    Update adminbuysViewer.vue.js

--- src/js/adminbuysViewer.vue.js
+++ src/js/adminbuysViewer.vue.js
@@ -241,7 +241,7 @@ const AdminbuysViewer = {
             <div class="card">
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
+                        <table class="table align-items-center table-hover mb-0">
                             <thead>
                                 <tr>
                                     <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Orden</th>
