046e533731fcad0387cf90cc734f6e19b04670d5
javierfernandez93 <javier.fernandez.pa93@gmail.com>
2024-08-07 23:20:52 -0600

    feat: mailing

--- apps/applications/view/paybusiness.view.php
+++ apps/applications/view/paybusiness.view.php
@@ -1,14 +1,16 @@
-<img src="<?php echo HCStudio\Connection::getMainPath();?>/src/img/mail-welcome.png" style="width: 100%;" alt="mail" title="mail"/>
+<img src="<?php echo HCStudio\Connection::getMainPath();?>/src/img/activation.png" style="width: 100%;" alt="mail" title="mail"/>
 
-<h2>Hola <b><?php echo $names; ?></b> felices de tenerte con nosotros</h2>
+<h3>Estimado <b><?php echo $names; ?></b> </h3>
 
-<h3>
-    Por favor para verificar tu cuenta haz clic <a href="<?php echo HCStudio\Connection::getMainPath(); ?>/apps/login/verify?secret=<?php echo $secret;?>&email=<?php echo $email;?>">aquí</a>
-</h3>
+<h3>¡Bienvenido a tu membresía educativa y de negocios!</h3>
 
-<h3>
-    Bienvenido a esta maravillosa aventura. 
-</h3>
+<h3>Estamos encantados de que hayas activado tu cuenta y te unas a nuestra plataforma de aprendizaje.</h3>
+<h3>Ahora, estás listo para acceder a una amplia gama de información y recursos que te ayudarán a alcanzar tus metas educativas y profesionales; También podrás construir carrera con nosotros e ir por esos rangos que te ofrecen poderosas ganancias construyendo y liderando equipos de trabajo. </h3>
+
+<h3>Si tienes alguna pregunta o necesitas asistencia, no dudes en ponerte en contacto con nuestro equipo de soporte en customerservice@unlimitedgroup.com o a través de nuestro servidor en Discord.</h3>
+
+<h3>Gracias por elegir DISRUPTIVO. ¡Esperamos que disfrutes tu experiencia de aprendizaje con nosotros!</h3>
+
 
 <h3>
     <b>
