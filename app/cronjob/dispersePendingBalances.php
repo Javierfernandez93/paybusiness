e7753fb0ee60907fe57e218dbf98ff1f38926646
javierfernandez93 <javier.fernandez.pa93@gmail.com>
2024-09-08 11:51:19 -0600

    feat: enhance dispersePendingBalances to validate credentials and optimize commission dispersal process with improved error handling

--- app/cronjob/dispersePendingBalances.php
+++ app/cronjob/dispersePendingBalances.php
@@ -2,65 +2,58 @@
 
 require_once TO_ROOT. "/system/core.php";
 
-$data = HCStudio\Util::getParam();
-
 $UserSupport = new Site\UserSupport;
 
-$data['PHP_AUTH_USER'] = $data['PHP_AUTH_USER'] ?? false;
-$data['PHP_AUTH_PW'] = $data['PHP_AUTH_PW'] ?? false;
+if(!validateCronCredentials() && !$UserSupport->logged) {
+    endWebServiceWithUnauthorized(Constants::RESPONSES['INVALID_CREDENTIALS'],printErrorFromCronjob(),true);
+}
 
-if(($data['PHP_AUTH_USER'] == HCStudio\Util::USERNAME && $data['PHP_AUTH_PW'] == HCStudio\Util::PASSWORD) || $UserSupport->logged === true)
+$CommissionPerUser = new Site\CommissionPerUser;
+$MembershipPerUser = new Site\MembershipPerUser;
+$UserLogin = new Site\UserLogin(false,false);
+$BuyPerUser = new Site\BuyPerUser;
+
+$dispertions = [];
+
+$Wallet = BlockChain\Wallet::getWallet(BlockChain\Wallet::MAIN_EWALLET);
+
+if($commissions = $CommissionPerUser->getPendingCommissions())
 {
-    $CommissionPerUser = new Site\CommissionPerUser;
-    $MembershipPerUser = new Site\MembershipPerUser;
-    $UserLogin = new Site\UserLogin(false,false);
-    $BuyPerUser = new Site\BuyPerUser;
-    
-    $dispertions = [];
-    
-    if($commissions = $CommissionPerUser->getPendingCommissions())
+    foreach($commissions as $commission)
     {
-        foreach($commissions as $commission)
+        if($UserLogin->_hasProductPermission('pay_business',$commission['user_login_id']))
         {
-            if($UserLogin->_hasProductPermission('pay_business',$commission['user_login_id']))
+            if($MembershipPerUser->_hasMembershipSpace($commission['user_login_id']))
             {
-                if($MembershipPerUser->_hasMembershipSpace($commission['user_login_id']))
-                {
-                    $message = 'COMISIÓN';
-        
-                    if($transaction_per_wallet_id = send([
-                        'status' => $commission['status'],
-                        'buy_per_user_id' => $commission['buy_per_user_id'],
-                        'user_login_id' => $commission['user_login_id'],
-                        'amount' => $commission['amount'],
-                        'message' => $message,
-                    ],$BuyPerUser))
-                    {
-                        $dispertions[] = $commission;
+                $message = 'COMISIÓN';
     
-                        $CommissionPerUser::setCommissionAsDispersed($commission['commission_per_user_id'],$transaction_per_wallet_id);
-        
-                        sendPush($commission['user_login_id'],"Hemos dispersado $ ".number_format($commission['amount'],2)." USD a tu ewallet.",Site\CatalogNotification::GAINS);
-                    }
+                if($transaction_per_wallet_id = send([
+                    'status' => $commission['status'],
+                    'buy_per_user_id' => $commission['buy_per_user_id'],
+                    'user_login_id' => $commission['user_login_id'],
+                    'amount' => $commission['amount'],
+                    'message' => $message,
+                ],$BuyPerUser,$Wallet))
+                {
+                    $dispertions[] = $commission;
+
+                    $CommissionPerUser::setCommissionAsDispersed($commission['commission_per_user_id'],$transaction_per_wallet_id);
+    
+                    sendPush($commission['user_login_id'],"Hemos dispersado $ ".number_format($commission['amount'],2)." USD a tu ewallet.",Site\CatalogNotification::GAINS);
                 }
             }
         }
     }
-
-    $data['dispertions'] = $dispertions;
-    $data['s'] = 1;
-    $data['r'] = "DATA_OK";
-} else {
-    $data['s'] = 0;
-    $data['r'] = "INVALID_CREDENTIALS";
 }
+
+$data['dispertions'] = $dispertions;
 
 function sendPush(string $user_login_id = null,string $message = null,int $catalog_notification_id = null) : bool
 {
     return Site\NotificationPerUser::push($user_login_id,$message,$catalog_notification_id,"");
 }
 
-function send(array $data = null,$BuyPerUser = null)
+function send(array $data = null,$BuyPerUser = null,$Wallet = null)
 {
     $wallet_kind_id = $data['status'] == Site\CommissionPerUser::FROZEN ? BlockChain\WalletKind::USDT_NOWITHDRAWABLE : BlockChain\WalletKind::USDT_TRC20;
 
@@ -85,8 +78,6 @@ function send(array $data = null,$BuyPerUser = null)
     {
         if($data['amount'])
         {
-            $Wallet = BlockChain\Wallet::getWallet(BlockChain\Wallet::MAIN_EWALLET);
-
             if($transaction_per_wallet_id = $Wallet->createTransaction($ReceiverWallet->public_key,$data['amount'],BlockChain\Transaction::prepareData(['@optMessage'=>$data['message']]),true))
             {
                 return $transaction_per_wallet_id;
@@ -95,4 +86,8 @@ function send(array $data = null,$BuyPerUser = null)
     } 
 }
 
-echo json_encode(HCStudio\Util::compressDataForPhone($data)); 
+endWebServiceWithSuccess(Constants::RESPONSES['CRONJOB_EJECUTED'],[
+    'dispertions' => $dispertions,
+    'code' => 'gains',
+    'mode' => $UserSupport->logged ? 'user' : 'cron'
+],true);
