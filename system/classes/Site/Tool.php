dd2d8828e5af44eca616946f2c231813619f7b99
javier <leqjl93@hotmail.com>
2024-09-08 16:56:55 -0600

    fix: change Tool status filter to include only active records for accurate data retrieval in Site tool queries

--- system/classes/Site/Tool.php
+++ system/classes/Site/Tool.php
@@ -36,7 +36,7 @@ class Tool extends Orm {
             ON 
               user_support.user_support_id = {$this->tblName}.user_support_id
             WHERE
-              {$this->tblName}.status != '-1'
+              {$this->tblName}.status = '1'
             GROUP BY 
               {$this->tblName}.{$this->tblName}_id
             ";
